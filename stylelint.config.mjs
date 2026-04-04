ed32210ed5f10fe40d14468b01f94fcc92b2800d
Vladimir Nikulin <mail.jorey@gmail.com>
2026-04-04 17:00:06 +0300

    chore: setup stylelint and his `stylelint.config,mjs`

---
+++ stylelint.config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('stylelint').Config} */
+/* stylelint-disable-next-line selector-type-no-unknown */
+export default {
+    /* stylelint-disable-next-line property-no-unknown */
+    extends: ["stylelint-config-standard-scss"],
+};
