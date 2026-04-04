b766ce4387fdfa655c9ddbae02ab5ab78ebfce84
Vladimir Nikulin <mail.jorey@gmail.com>
2026-04-04 20:28:30 +0300

    chore(eslint): enabled format with prettier

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,20 +1,29 @@
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+import { defineConfig } from 'eslint/config'
+
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierConfig from 'eslint-config-prettier'
+
+import prettierRecomended from 'eslint-plugin-prettier/recommended'
 
 export default defineConfig([
     {
-        files: ["**/*.{js,mjs,cjs,ts,mts,cts}"],
-        plugins: { js },
-        extends: ["js/recommended"],
+        files: ['**/*.{js,mjs,cjs,ts,mts,cts}'],
+        plugins: {
+            js,
+            prettier: prettierPlugin,
+        },
+        extends: ['js/recommended', prettierConfig],
         rules: {
-            "no-console": "warn",
-            eqeqeq: "warn",
-            curly: "warn",
-            "no-else-return": "warn",
+            ...prettierRecomended.rules,
+            'no-console': 'warn',
+            'eqeqeq': 'warn',
+            'curly': 'warn',
+            'no-else-return': 'warn',
         },
         languageOptions: { globals: globals.browser },
     },
     tseslint.configs.recommended,
-]);
+])
