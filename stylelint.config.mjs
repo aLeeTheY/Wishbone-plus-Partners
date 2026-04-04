0662cc2acab040b86f71647844bc930bc661368c
Vladimir Nikulin <mail.jorey@gmail.com>
2026-04-04 20:27:10 +0300

    chore(stylelint): update stylelint config

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -1,6 +1,27 @@
 /** @type {import('stylelint').Config} */
-/* stylelint-disable-next-line selector-type-no-unknown */
 export default {
-    /* stylelint-disable-next-line property-no-unknown */
-    extends: ["stylelint-config-standard-scss"],
-};
+    extends: ['stylelint-config-standard-scss'],
+    plugins: ['stylelint-selector-bem-pattern'],
+    rules: {
+        'plugin/selector-bem-pattern': {
+            componentName: '[a-z0-9]+(?:-[a-z0-9]+)*',
+            componentSelectors: {
+                initial: '^\\.{componentName}(?:__[a-z0-9]+)?(?:--[a-z0-9]+)?$',
+            },
+            utilitySelectors: '^\\.util-[a-z]+$',
+        },
+        'selector-class-pattern': null,
+        'number-max-precision': 3,
+        // 'scss/double-slash-comment-whitespace-inside': '^\\/\\/[!*?todo] ([a-z0-9])*$',
+        'scss/at-mixin-pattern': '^([a-z0-9]+(-[a-z0-9]+)*)(--[a-z0-9]+)?$',
+    },
+    ignoreFiles: [
+        '**/*.js',
+        '**/*.mjs',
+        '**/*.cjs',
+        '**/*.ts',
+        '**/*.mts',
+        '**/*.cts',
+        '**/vendors/*.scss',
+    ],
+}
