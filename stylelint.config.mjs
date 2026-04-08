61d17d9cc22f4b7a04b975b9968824e2cc598272
Vladimir Nikulin <mail.jorey@gmail.com>
2026-04-08 17:48:55 +0300

    chore(stylelint): updated Stylelint config file

--- stylelint.config.mjs
+++ stylelint.config.mjs
@@ -13,10 +13,11 @@ export default {
             {
                 // * plugin reads 'production' section by default from .browserslistrc
                 // browsers: ['last 4 versions', '> 0.2%', 'not dead'],
-                ignore: ['css-nesting'],
+                ignore: ['css-nesting', 'multicolumn'],
                 severity: 'warning',
             },
         ],
+        'scss/double-slash-comment-empty-line-before': null,
         'media-feature-range-notation': 'prefix',
         'declaration-empty-line-before': null,
         'selector-class-pattern': null,
